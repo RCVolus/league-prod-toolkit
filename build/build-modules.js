7130acd4c1c1946ef5e2986aa32c0af304a2b3cf
Lars Bärtschi <lars.baertschi@swisscom.com>
2021-06-01 13:32:57 +0200

    fix linting of build script

--- build/build-modules.js
+++ build/build-modules.js
@@ -1,68 +1,67 @@
-const fs = require('fs');
-const { promisify } = require('util');
-const path = require('path');
-const { exec } = require('child_process'); 
-const console = require('console');
+const fs = require('fs')
+const { promisify } = require('util')
+const path = require('path')
+const { exec } = require('child_process')
+const console = require('console')
 
-const readdirPromise = promisify(fs.readdir);
+const readdirPromise = promisify(fs.readdir)
 const readFilePromise = promisify(fs.readFile)
-const statPromise = promisify(fs.stat);
-const execPromise = promisify(exec);
+const statPromise = promisify(fs.stat)
+const execPromise = promisify(exec)
 
-const modulePath = './modules';
+const modulePath = './modules'
 
-let filter = ""
+let filter = ''
 if (process.argv.length === 3) {
-    filter = process.argv[2]
+  filter = process.argv[2]
 }
 
 const main = async () => {
-    const data = await readdirPromise(modulePath);
-    await Promise.all(
-        data.map(async (folderName) => {
-            if (filter !== "" && folderName !== filter) {
-                return;
-            }
+  const data = await readdirPromise(modulePath)
+  await Promise.all(
+    data.map(async (folderName) => {
+      if (filter !== '' && folderName !== filter) {
+        return
+      }
 
-            const currentModulePath = path.join(modulePath, folderName)
-            const packageJsonPath = path.join(currentModulePath, 'package.json')
+      const currentModulePath = path.join(modulePath, folderName)
+      const packageJsonPath = path.join(currentModulePath, 'package.json')
 
-            try {
-                // Check that package.json exists
-                await statPromise(packageJsonPath)
-            } catch {
-                return;
-            }
+      try {
+        // Check that package.json exists
+        await statPromise(packageJsonPath)
+      } catch {
+        return
+      }
 
-            const pkgJson = JSON.parse((await readFilePromise(packageJsonPath)).toString());
+      const pkgJson = JSON.parse((await readFilePromise(packageJsonPath)).toString())
 
-            if (pkgJson['dependencies'] || pkgJson['devDependencies']) {
-                // run install
-                await execPromise('npm install', {
-                    cwd: currentModulePath
-                });
-
-                console.log('installed ' + folderName);
-            }
-
-
-            if (pkgJson['toolkit']['needsBuild']) {
-                // run build
-                await new Promise((resolve, reject) => {
-                    exec('npm run build', {
-                        cwd: currentModulePath
-                    }, (error, stdout, stderr) => {
-                        console.log(stdout)
-                        if (error || stderr) {
-                            reject(error || stderr)
-                        }
-                        console.log('built ' + folderName);
-                        resolve();
-                    });
-                });
-            }
+      if (pkgJson.dependencies || pkgJson.devDependencies) {
+        // run install
+        await execPromise('npm install', {
+          cwd: currentModulePath
         })
-    );
+
+        console.log('installed ' + folderName)
+      }
+
+      if (pkgJson.toolkit.needsBuild) {
+        // run build
+        await new Promise((resolve, reject) => {
+          exec('npm run build', {
+            cwd: currentModulePath
+          }, (error, stdout, stderr) => {
+            console.log(stdout)
+            if (error || stderr) {
+              reject(error || stderr)
+            }
+            console.log('built ' + folderName)
+            resolve()
+          })
+        })
+      }
+    })
+  )
 }
 
-main();
+main()
