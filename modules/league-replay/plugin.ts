206b949fa0bc2edb9bc536b90bee8049d93204d5
Max <63208379+Himyu@users.noreply.github.com>
2021-06-13 15:11:52 +0200

    change tree and add replay

--- modules/league-replay/plugin.ts
+++ modules/league-replay/plugin.ts
@@ -1,6 +1,22 @@
-const namespace = 'league-live-events';
+const namespace = 'league-replay';
 
 module.exports = async (ctx: any) => {
+  ctx.LPTE.emit({
+    meta: {
+      type: 'add-pages',
+      namespace: 'ui',
+      version: 1
+    },
+    pages: [{
+      name: 'OP: league-replay',
+      frontend: 'frontend',
+      id : 'op-league-replay'
+    }]
+  });
+
+  ctx.LPTE.on(namespace, 'set-render', async (e: any) => {
+    // TODO handle data and show on map
+  });
 
   // Emit event that we're ready to operate
   ctx.LPTE.emit({
