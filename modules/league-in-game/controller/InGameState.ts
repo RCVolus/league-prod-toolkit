5675bbfc17df3deff394c4e65c141b41c0220dc1
Maximilian Bala <max_b98@hotmail.de>
2021-07-28 20:32:24 +0200

    add league-in-game plugin

---
+++ modules/league-in-game/controller/InGameState.ts
@@ -0,0 +1,84 @@
+import { json } from 'express'
+import type { PluginContext } from 'league-prod-toolkit/core/modules/Module'
+import { AllGameData, Player } from '../types/AllGameData'
+import { Config } from '../types/Config'
+import { ItemEpicness } from '../types/Items'
+
+export class InGameState {
+
+  public gameData : any[] = []
+  public itemEpicness : number[]
+
+  constructor (
+    private namespace: string,
+    private ctx: PluginContext,
+    private config: Config,
+    private statics: any
+  ) {
+    this.itemEpicness = this.config.items.map(i => ItemEpicness[i])
+  }
+
+  public handelData (allGameData: AllGameData) : void {
+    if (this.gameData.length > 0) {
+      const previousGameData = this.gameData[this.gameData.length -1]
+      this.checkPlayerUpdate(allGameData, previousGameData)
+    }
+
+    this.gameData.push(allGameData)
+  }
+
+  private checkPlayerUpdate (allGameData: AllGameData, previousGameData: AllGameData) {
+    if (this.config.items.length === 0) return
+    if (allGameData.allPlayers.length === 0 || previousGameData.allPlayers.length === 0) return
+
+    allGameData.allPlayers.forEach((player, i) => {
+      this.checkItemUpdate(player, previousGameData.allPlayers[i], i)
+      this.checkLevelUpdate(player, previousGameData.allPlayers[i], i)
+    })
+  }
+
+  private checkItemUpdate (currentPlayerState: Player, previousPlayerState: Player, id: number) {
+    if (previousPlayerState === undefined) return
+    if (currentPlayerState.level === previousPlayerState.level) return
+    if (!this.config.level.includes(currentPlayerState.level.toString())) return
+
+    this.ctx.LPTE.emit({
+      meta: {
+        type: 'levelUpdate',
+        namespace: this.namespace,
+        version: 1
+      },
+      team: currentPlayerState.team,
+      player: id,
+      level: currentPlayerState.level
+    })
+  }
+
+  private checkLevelUpdate (currentPlayerState: Player, previousPlayerState: Player, id: number) {
+    if (previousPlayerState === undefined) return
+
+    const previousItems = previousPlayerState.items
+
+    for (const item of currentPlayerState.items) {
+
+      const itemID = item.itemID
+      if (previousItems.find(i => i.itemID === itemID)) continue
+
+      const itemBinFind = this.statics.itemBin.find((i: any) => i.itemID === itemID)
+      if (itemBinFind === undefined) continue
+
+      if (!this.itemEpicness.includes(itemBinFind.epicness)) continue
+
+      this.ctx.LPTE.emit({
+        meta: {
+          type: 'itemUpdate',
+          namespace: this.namespace,
+          version: 1
+        },
+        team: currentPlayerState.team,
+        player: id,
+        item: this.statics.staticURL + "/img/item/" + itemID + ".png"
+      })
+    }
+  }
+}
---
+++ modules/league-in-game/frontend/gfx/ingame.js
@@ -0,0 +1,56 @@
+const namespace = 'league-in-game';
+const blueTeam = document.querySelector('#blue')
+const redTeam = document.querySelector('#red')
+
+function getPlayerId(id) {
+  if (id > 4) return id - 5
+  else return id
+}
+
+function levelUpdate (e) {
+  console.log(e)
+  const playerId = getPlayerId(e.player)
+
+  const team = e.team === "ORDER" ? blueTeam : redTeam
+  const playerDiv = team.children[playerId]
+
+  const levelContainer = playerDiv.querySelector('.level')
+
+  if (playerDiv.classList.contains('levelUp') || playerDiv.classList.contains('itemBuy')) {
+    return setTimeout(() => {
+      levelUpdate(e)
+    }, 3000)
+  }
+
+  levelContainer.innerHTML = e.level
+  playerDiv.classList.add('levelUp')
+  setTimeout(() => {
+    playerDiv.classList.remove('levelUp')
+  }, 6000)
+}
+
+function itemUpdate (e) {
+  const playerId = getPlayerId(e.player)
+
+  const team = e.team === "ORDER" ? blueTeam : redTeam
+  const playerDiv = team.children[playerId]
+
+  const levelContainer = playerDiv.querySelector('.item')
+
+  if (playerDiv.classList.contains('levelUp') || playerDiv.classList.contains('itemBuy')) {
+    return setTimeout(() => {
+      itemUpdate(e)
+    }, 3000)
+  }
+
+  levelContainer.src = e.item
+  playerDiv.classList.add('itemBuy')
+  setTimeout(() => {
+    playerDiv.classList.remove('itemBuy')
+  }, 6000)
+}
+
+LPTE.onready(async () => {
+  LPTE.on(namespace, 'levelUpdate', levelUpdate);
+  LPTE.on(namespace, 'itemUpdate', itemUpdate);
+})
