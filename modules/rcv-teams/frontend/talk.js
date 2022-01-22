07fc1a4c6041e9e87e6d75305dcef6218d5ef050
Himyu <max_b98@hotmail.de>
2022-01-22 13:27:00 +0100

    add points to team in talk

--- modules/rcv-teams/frontend/talk.js
+++ modules/rcv-teams/frontend/talk.js
@@ -3,6 +3,7 @@ const blueTag = document.querySelector('#blue-tag')
 const redTag = document.querySelector('#red-tag')
 const blueName = document.querySelector('#blue-name')
 const redName = document.querySelector('#red-name')
+const pointContainer = document.querySelector('#point-container')
 
 const tick = async () => {
   const data = await window.LPTE.request({
@@ -25,9 +26,6 @@ const tick = async () => {
 const update = (data) => {
   if (data.state === "READY") {
     displayTeams(data.teams, data.bestOf)
-  } else {
-    tagContainer.style.display = 'none'
-    pointContainer.style.display = 'none'
   }
 }
 
@@ -56,6 +54,9 @@ function displayTeams(teams, bestOf) {
   if (teams.blueTeam.score === 0 && teams.redTeam.score === 0) return
 
   if (bestOf > 1) {
+    document.body.classList.add('has-scores')
+    pointContainer.style.display = 'flex'
+    displayPoints(bestOf, teams.blueTeam, teams.redTeam)
     if (teams.blueTeam.score > bestOf / 2) {
       redTag.classList.add('outline')
       redName.classList.add('outline')
@@ -64,6 +65,8 @@ function displayTeams(teams, bestOf) {
       blueName.classList.add('outline')
     }
   } else {
+    document.body.classList.remove('has-scores')
+    pointContainer.style.display = 'none'
     if (teams.blueTeam.score > teams.redTeam.score) {
       redTag.classList.add('outline')
       redName.classList.add('outline')
@@ -89,4 +92,37 @@ const resizeText = ( parent ) => {
 
   // revert to last state where no overflow happened:
   parent.style.fontSize = `${i - 1}px`
+}
+
+function displayPoints (bestOf, blueTeam, redTeam) {
+  const pointsToWin = Math.ceil(bestOf / 2)
+  for (let i = 0; i < 5; i++) {
+    const point = i + 1
+
+    const setTeamPoints = (teamName, teamData) => {
+      const selector = document.getElementById(`point-${teamName}-${point}`)
+      if (teamData.score >= point) {
+        // Point scored, make visible
+        selector.style.display = 'block'
+        // selector.style.visibility = 'unset'
+        selector.classList.remove('empty')
+      } else {
+        // is this point possible to make?
+        if (point > pointsToWin) {
+          // no, completely not display
+          selector.style.display = 'none'
+          // selector.style.visibility = 'unset'
+          selector.classList.remove('empty')
+        } else {
+          // yes, only soft hide
+          // selector.style.visibility = 'hidden'
+          selector.style.display = 'block'
+          selector.classList.add('empty')
+        }
+      }
+    }
+
+    setTeamPoints('blue', blueTeam)
+    setTeamPoints('red', redTeam)
+  }
 }
