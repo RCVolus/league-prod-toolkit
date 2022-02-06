3e3b0ca87d1e8010ebb921ad10c5d971d1fa2407
Maximilian Bala <max_b98@hotmail.de>
2022-02-06 13:05:15 +0000

    finish progress bar setup

--- core/logging/progress.ts
+++ core/logging/progress.ts
@@ -1,25 +1,47 @@
-import cliProgress, { MultiBar } from 'cli-progress'
+import cliProgress, { MultiBar, Options, Params } from 'cli-progress'
 
-const dateObj = new Date()
-const month = String(dateObj.getMonth()).padStart(2, '0')
-const day = String(dateObj.getDate()).padStart(2, '0')
-const year = dateObj.getFullYear()
-const hours = String(dateObj.getHours()).padStart(2, '0')
-const minutes = String(dateObj.getMinutes()).padStart(2, '0')
-const seconds = String(dateObj.getSeconds()).padStart(2, '0')
-const milliSeconds = String(dateObj.getMilliseconds()).padStart(4, '0')
-const output = `${year}-${month}-${day}T${hours}-${minutes}-${seconds}.${milliSeconds}Z`
+const progress = (plugin: string): MultiBar => {
+  const currentDate = (): string => {
+    const dateObj = new Date()
+    const month = String(dateObj.getMonth()).padStart(2, '0')
+    const day = String(dateObj.getDate()).padStart(2, '0')
+    const year = dateObj.getFullYear()
+    const hours = String(dateObj.getHours()).padStart(2, '0')
+    const minutes = String(dateObj.getMinutes()).padStart(2, '0')
+    const seconds = String(dateObj.getSeconds()).padStart(2, '0')
+    const milliSeconds = String(dateObj.getMilliseconds()).padStart(3, '0')
+    return `${year}-${month}-${day}T${hours}:${minutes}:${seconds}.${milliSeconds}Z`
+  }
 
-function formatter (plugin: string): string {
-  return `${output} [\u001b[32m${'Info'.padEnd(5)}\u001b[39m] ${`\u001b[95m${plugin}\u001b[39m`.padEnd(
-    22
-  )}: [{bar}] {percentage}% | {value}/{total}`
+  function formatter (options: Options, params: Params, payload: any): string {
+    const percent = Math.round((params.value / params.total) * 100)
+
+    const length = 20
+
+    const completeSize = Math.round(params.progress * length)
+    const incompleteSize = length - completeSize
+    const bar = (options.barCompleteString ?? '\u2588').substring(0, completeSize) +
+    (options.barIncompleteString ?? '\u2591').substring(0, incompleteSize)
+
+    const msg = `${currentDate()} [\u001b[32m${'Info'.padEnd(5)}\u001b[39m] ${`\u001b[95m${plugin}\u001b[39m`.padEnd(
+      22
+    )}: ${payload.task as string} [${bar}] ${percent}%`
+
+    if (params.value >= params.total) {
+      return msg + '\n'
+    } else return msg
+  }
+
+  return new cliProgress.MultiBar({
+    clearOnComplete: false,
+    hideCursor: true,
+    format: formatter,
+    barCompleteChar: '\u2588',
+    barIncompleteChar: '\u2591',
+    linewrap: false,
+    stopOnComplete: true,
+    forceRedraw: true
+  }, cliProgress.Presets.shades_grey)
 }
-
-const progress = (plugin: string): MultiBar => new cliProgress.MultiBar({
-  clearOnComplete: false,
-  hideCursor: true,
-  format: formatter(plugin)
-}, cliProgress.Presets.shades_grey)
 
 export default progress
