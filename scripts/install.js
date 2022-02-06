70d7137bc5bed9e0f7182f99f7fb027b4ace9c1f
Maximilian Bala <max_b98@hotmail.de>
2022-02-06 23:40:27 +0000

    add new installer and removed region from setup

--- scripts/install.js
+++ scripts/install.js
@@ -1,19 +1,66 @@
 const fs = require('fs')
 const path = require('path')
+const inquirer = require('inquirer')
+const { createSpinner } = require('nanospinner')
 
-const apiKey = process.argv[3]
-const region = process.argv[4]
-const server = process.argv[5]
+const getApiKey = async () => {
+  const apiKey = await inquirer.prompt({
+    type: 'input',
+    name: 'apiKey',
+    message: 'Enter your Riot-API-Key (RGAPI-SECRETKEY)',
+    default: 'RGAPI-SECRETKEY'
+  })
+
+  return apiKey.apiKey
+}
+
+const getServer = async () => {
+  const server = await inquirer.prompt({
+    type: 'list',
+    name: 'server',
+    message: 'Enter your server (EUW1)',
+    default: 'EUW1',
+    choices: [
+      'BR1',
+      'EUN1',
+      'EUW1',
+      'JP1',
+      'KR',
+      'LA1',
+      'LA2',
+      'NA1',
+      'OC1',
+      'TR1',
+      'RU',
+    ]
+  })
+
+  return server.server
+}
 
 const filePath = path.join(__dirname, '..', 'modules', 'plugin-config', 'config.dist.json')
 const newFilePath = path.join(__dirname, '..', 'modules', 'plugin-config', 'config.json')
 const file = require(filePath)
 
-file['provider-webapi'].apiKey = apiKey
-file['provider-webapi'].region = region
-file['provider-webapi'].server = server
+const askQuestions = async () => {
+  const apiKey = await getApiKey()
+  const server = await getServer()
 
-fs.writeFile(newFilePath, JSON.stringify(file, null, 2), function (err) {
-  if (err) return console.log(err)
-  console.log('Saving config ...')
-})
+  file['provider-webapi'].apiKey = apiKey
+  file['provider-webapi'].server = server
+
+  const spinner = createSpinner('Saving config')
+
+  try {
+    await fs.promises.writeFile(newFilePath, JSON.stringify(file, null, 2))
+    spinner.success({
+      text: 'config saved'
+    })
+  } catch (err) {
+    spinner.error({
+      text: err.message
+    })
+  }
+}
+
+askQuestions()
