75d6c6f03613046e89a437505c8962f7840cd490
Maximilian Bala <max_b98@hotmail.de>
2022-04-05 16:08:40 +0100

    change setup file paths in installer

--- install.bat
+++ install.bat
@@ -2,9 +2,9 @@
 
 powershell -Command "& {npm i --production}"
 
-powershell -Command "& {node ./scripts/install.js -plugins theme-default}"
+powershell -Command "& {node ./dist/scripts/install.js -plugins theme-default}"
 
-powershell -Command "& {node ./scripts/setup.js}"
+powershell -Command "& {node ./dist/scripts/setup.js}"
 
 echo Installation complete! You can now start the Tool by just executing the 'start.bat'
 pause
