c0c0723446554bb9b5171ddf78b3c3e6acf3ad82
Maximilian Bala <max_b98@hotmail.de>
2022-04-09 17:25:49 +0100

    update module page

--- core/web/controller/modules.ts
+++ core/web/controller/modules.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express'
 
 import moduleService from '../../modules/ModuleService'
+import { download } from '../../../scripts/install'
 
 export default (globalContext: any): Router => {
   const router = Router()
@@ -15,15 +16,20 @@ export default (globalContext: any): Router => {
           Plugins: moduleService.modules.filter(m => m.getName().startsWith('plugin')),
           'League Modules': moduleService.modules.filter(m => m.getName().includes('league')),
           'Valo Modules': moduleService.modules.filter(m => m.getName().includes('valo')),
-          Themes: moduleService.modules.filter(m => m.getName().startsWith('theme'))
+          Themes: moduleService.modules.filter(m => m.getName().startsWith('theme')),
+          Other: moduleService.modules.filter(m =>
+            !m.getName().startsWith('theme') && !m.getName().includes('valo') && !m.getName().includes('league') && !m.getName().startsWith('plugin'))
         },
         availableModuleCount: moduleService.assets.length,
         availableModules: {
           Plugins: moduleService.assets.filter(m => m.name.startsWith('plugin')),
           'League Modules': moduleService.assets.filter(m => m.name.includes('league')),
           'Valo Modules': moduleService.assets.filter(m => m.name.includes('valo')),
-          Themes: moduleService.assets.filter(m => m.name.startsWith('theme'))
-        }
+          Themes: moduleService.assets.filter(m => m.name.startsWith('theme')),
+          Other: moduleService.assets.filter(m =>
+            !m.name.startsWith('theme') && !m.name.includes('valo') && !m.name.includes('league') && !m.name.startsWith('plugin'))
+        },
+        download
       })
   })
   router.get('/api', (req, res) => {
