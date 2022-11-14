46ae3e00cdf4727c38839e873460f68fa222760d
himyu <max_b98@hotmail.de>
2022-11-14 11:13:50 +0100

    add version checker

--- core/app.ts
+++ core/app.ts
@@ -1,4 +1,6 @@
 import minimist from 'minimist'
+import { gt } from 'semver'
+import { version } from '../package.json'
 
 const argv = minimist(process.argv.slice(2))
 
@@ -12,6 +14,7 @@ import logger, { eventbusTransport } from './logging'
 import { runServer } from './web/server'
 import moduleService from './modules/ModuleService'
 import lpteService from './eventbus/LPTEService'
+import axios from 'axios'
 /* eslint-enable */
 
 const log = logger('main')
@@ -23,8 +26,25 @@ log.info('| |__| (_) | |___    | | (_) | (_) | |   <| | |_ ')
 log.info('|_____\\___/|_____|   |_|\\___/ \\___/|_|_|\\_\\_|\\__|')
 log.info('')
 
+const checkVersion = async () => {
+  const res = await axios.get('https://prod-toolkit-latest.himyu.workers.dev/')
+
+  if (res.status !== 200) {
+    return log.warn('The current version could not be checked')
+  }
+
+  if (gt(version, res.data.tag_name)) {
+    log.info('='.repeat(50))
+    log.info(`There is a new version available: ${res.data.tag_name as string}`)
+    log.info('='.repeat(50))
+    log.info('')
+  }
+}
+
 const main = async (): Promise<void> => {
-  await lpteService.initialize()
+  await checkVersion()
+
+  lpteService.initialize()
   eventbusTransport.lpte = lpteService
 
   await moduleService.initialize()
