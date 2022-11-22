c6d00458ead9f81c7fe8422ecacb7e92838ae1b4
Himyu <max_b98@hotmail.de>
2022-11-22 14:59:58 +0100

    update lint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,5 @@
 module.exports = {
   extends: 'standard-with-typescript',
-  parserOptions: {
-    project: './tsconfig.json'
-  },
   ignorePatterns: ['*.pug', '*.png', '*.bat', '*.md', '*.json', 'LICENSE'],
   rules: {
     '@typescript-eslint/no-misused-promises': 'off'
