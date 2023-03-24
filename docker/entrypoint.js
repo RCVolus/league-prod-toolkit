9dbd804813f1a6fcb3f870a443f7ca369118f96e
Laraka | Lara Bärtschi <info@lara.ninja>
2023-03-24 13:12:53 +0000

    add docker action to automatically build docker image

--- docker/entrypoint.js
+++ docker/entrypoint.js
@@ -10,7 +10,7 @@ const onExit = (childProcess) => {
       if (code === 0) {
         resolve(undefined)
       } else {
-        reject(new Error('Exit with error code: ' + code));
+        reject(new Error('Exit with error code: ' + code))
       }
     })
     childProcess.once('error', (err) => {
